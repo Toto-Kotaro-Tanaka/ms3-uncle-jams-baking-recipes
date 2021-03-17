c092d61a31df26afc105f9b54491768e58cf6c4d
Toto-Kotaro-Tanaka <ktntrtmt@gmail.com>
2021-03-17 15:33:12 +0000

    Create register.html

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -27,6 +28,11 @@ def home():
 @app.route("/categories")
 def categories():
     return render_template("categories.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
